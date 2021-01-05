5d94f14d5f8987a145ee76ad18e2387baccb0888
rahuljha661 <rahuljha12527@gmail.com>
2021-01-06 00:09:11 +0530

    add bcrypt routes

---
+++ routes/auth.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const bcrypt=require('bcryptjs');
+
+
+router.post("/signup", (req, res) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  if (!email || !password || !name) {
+    return res.status(422).json({
+      error: "please add all the fields",
+    });
+  }
+
+  User.findOne({ email: email }).then((savedUser) => {
+    if (savedUser) {
+      return res.status(422).json({
+        error: "user already exists with that email",
+      });
+    }
+    bcrypt.hash(password,12)
+    .then(hashedpassword=>{
+      const user=new User({
+        email,
+        password:hashedpassword,
+        name
+      })
+  
+      user.save()
+        .then(user=>{
+          res.json({message:"saved successfully"})
+        })
+        .catch(err=>{
+          console.log(err);
+        })
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+        
+    })
+//  res.json({ message: "successfully posted" });
+
+})
+
+module.exports = router;
