fb821307328b9bbbe2f0e32719612c873648a902
rahuljha661 <rahuljha12527@gmail.com>
2021-01-07 00:16:45 +0530

    add auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -45,4 +45,32 @@ router.post("/signup", (req, res) => {
 
 })
 
+
+router.post("/signin",(req,res)=>{
+  const {email,password}=req.body
+
+  if(!email || !password){
+    return  res.status(422).json({error:"please add email and password"})
+  }
+  User.findOne({email:email})
+  .then(savedUser=>{
+    if(!savedUser){
+     return  res.status(422).json({error:"Invalid Email or password"})
+    }
+    bcrypt.compare(password,savedUser.password)
+     .then(doMatch=>{
+       if(doMatch){
+         res.json({message:"successfully signed in"})
+       }else{
+        return  res.status(422).json({error:"Invalid Email or password"})
+       }
+     })
+     .catch(err=>{
+       console.log(err)
+     })
+
+  })
+})
+
+
 module.exports = router;
