91669831f3dec6c0bb02c85db70a6631b28561e2
rahuljha661 <rahuljha12527@gmail.com>
2021-01-09 20:25:01 +0530

    add populate posts

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const PORT=5000;
 const {MONGOURI}=require('./keys');
 
 
-require('./models/user');
-app.use(express.json());
 
-app.use(require('./routes/auth'));
 
 
 mongoose.connect(MONGOURI,{
@@ -23,6 +20,15 @@ mongoose.connection.on('connected',()=>{
 mongoose.connection.on('error',(err)=>{
     console.log("err connecting ",err);
 })
+
+require('./models/user');
+require('./models/post');
+
+
+app.use(express.json());
+
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
 
 
 
