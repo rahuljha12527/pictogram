f733e450d2cb79ba3a79dfe0b80d74ad758f9b29
rahuljha661 <rahuljha12527@gmail.com>
2021-01-14 20:15:40 +0530

    worked on sign in page

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -32,6 +32,8 @@ const SignUp = () => {
         M.toast({html:data.message,classes:"#43a047 green darken-1"})
         history.push('/signin')
       }
+    }).catch(err=>{
+      console.log(err);
     })
   }
 
